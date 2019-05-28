f96456f2333fb0e52e6c5732cb043f796b76f060
Nabin Thomas <nabin@youknowwhere>
2019-05-28 12:53:01 -0700

    add import back.

--- setup/startservers.sh
+++ setup/startservers.sh
@@ -9,7 +9,7 @@ service mongodb start
 #For some reason unzip is never getting packaged in the Docker image. So installing it again temporarily. 
 apt-get install -y unzip 
 unzip -o app/server/dbsetup/data/data.zip -d app/server/dbsetup/data
-#PYTHONPATH=/root/app/ python3 app/server/dbsetup/populateDataBase.py  app/server/dbsetup/data/listings.csv app/server/dbsetup/data/reviews.csv app/server/dbsetup/data/listings_schema.json 
+PYTHONPATH=/root/app/ python3 app/server/dbsetup/populateDataBase.py  app/server/dbsetup/data/listings.csv app/server/dbsetup/data/reviews.csv app/server/dbsetup/data/listings_schema.json 
 rm app/server/dbsetup/data/*.csv
 #Start the web server
 #This should be started at the end
