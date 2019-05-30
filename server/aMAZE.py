60e41b9a09848a38d53d0676687e054b96a958f8
Nabin Thomas <nabin@youknowwhere>
2019-05-29 23:08:31 -0700

    make debug

--- server/aMAZE.py
+++ server/aMAZE.py
@@ -627,4 +627,5 @@ if __name__ == '__main__':
     db = mongo_client.get_database()
 
     ## Start the http server
-    app.run(host='0.0.0.0', port=443, ssl_context='adhoc', debug=False);
+    app.run(host='0.0.0.0', port=443, ssl_context='adhoc');
+
