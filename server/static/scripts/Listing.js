ea7b4a8b3ce04f65bcc2c38f09b1aa0f262c4ce9
Nabin Thomas <nabin@youknowwhere>
2019-05-31 11:36:28 -0700

    Listing Name added in UI

--- server/static/scripts/Listing.js
+++ server/static/scripts/Listing.js
@@ -7,7 +7,7 @@ import * as cookies from '/static/scripts/cookies.js';
 // Step1 : Create the listing element class:
 class ListingIDElement extends React.Component {
     constructor(props) {
-        console.log("ListingIDElement Props was " + JSON.stringify(props))
+        // console.log("ListingIDElement Props was " + JSON.stringify(props))
         super(props);
         this.state = {
             listingId : props['id']
@@ -15,23 +15,45 @@ class ListingIDElement extends React.Component {
         
     }
     render() {
-        console.log("ListingIDElement: Heading was " + this.state.listingId)
+        // console.log("ListingIDElement: Heading was " + this.state.listingId)
         return React.createElement('b', {align : "left"}, this.state.listingId)
     }
 }
+// Add the remaining UI elements' React classes below this section
+// Listing Name 
+class ListingNameElement extends React.Component {
+    constructor(props) {
+        console.log("ListingNameElement Props was " + JSON.stringify(props))
+        super(props);
+        this.state = {
+            listingName : props['name']
+        };
+        
+    }
+    render() {
+        console.log("ListingNameElement: Heading was " + this.state.listingName)
+        return React.createElement('b', {align : "left"}, this.state.listingName)
+    }
+}
+// Add the remaining UI elements' React classes above this section
 
 // Step 2: Create & Render the element which renders the part ( eg: ListingIDElement) . 
 const listingTableHeading = document.querySelector("#listings_table_heading")
 var listingTableHeadingElement = ReactDOM.render(React.createElement(ListingIDElement, {listingId : ''}), listingTableHeading);
 
+// Create and Render the remaining Elements under this. 
+const listingNameField = document.querySelector("#listing_name")
+var listingNameElement = ReactDOM.render(React.createElement(ListingNameElement, {listingName : ''}), listingNameField);
 
-// Add the remaining Elements under this section. 
 
-// Add the remaining UI elements above this section
+
+
+// Create and Render the remaining Elements above this. 
 
 // Step 3: Add the element which hold this data in HTML and find the ID here. 
 var listingElements = {
-    heading : listingTableHeadingElement
+    heading : listingTableHeadingElement,
+    name : listingNameElement
 };
 
 
@@ -78,19 +100,25 @@ class CompleteListingData extends React.Component {
         let rows = [];
         var thisRow = React.createElement('tr', { key: "t" }, JSON.stringify(this.state.listing) );
         rows.push(thisRow);
-        console.log("CompleteListingData Name from props = " + this.state.elements.name)
-        console.log(" CompleteListingData listing here was  = " +  this.state.elements.heading.state.listingData);
         
+        // console.log(" CompleteListingData listing here was  = " +  this.state.elements.heading.state.listingData);
+    
         // Step 4: Update the individual elements in the Web page
         if (this.state.elements.heading) {
             this.state.elements.heading.setState({listingId: this.state.listing.id})
         }
+        
+        if (this.state.elements.name) {
+            this.state.elements.name.setState({listingName: this.state.listing.name})
+        }
+        console.log("CompleteListingData Name from props = " + this.state.elements.name)
+
         var reviews = this.state.reviews;
         for (var i = 0; i < reviews.length; i++) {
-            var thisRow = React.createElement('tr', { key: i }, JSON.stringify( reviews[i] ) );
+            var thisRow = React.createElement('tr', { key: i, style : {width: "1000px"} }, JSON.stringify( reviews[i] ) );
             rows.push(thisRow);
         }
-        return null //rows;  
+        return rows;  
     }
 } //end class CompleteListingData
 
