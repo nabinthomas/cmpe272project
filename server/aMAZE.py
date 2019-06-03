581efb08ecedc3bbe8858caa0b778f3879f336dc
Nabin Thomas <nabin@youknowwhere>
2019-06-03 10:33:48 -0700

    Re-enable Auth requirement for rest APIs

--- server/aMAZE.py
+++ server/aMAZE.py
@@ -380,7 +380,7 @@ def clear_all_cookies(response):
 
 # eg: curl -XPOST -H 'Content-Type: application/json' https://0.0.0.0/api/listings -d '{"page_index" : 2 }'
 @app.route('/api/listings', methods=['POST'])
-#@requires_auth
+@requires_auth
 def get_listings():
     """ 
     Function to fetch listings 
@@ -538,7 +538,7 @@ def get_listings():
 
 # curl -XGET https://localhost/api/listings/9835
 @app.route('/api/listings/<string:listings_id>', methods=['GET'])
-#@requires_auth
+@requires_auth
 def get_one_listing(listings_id):
     """  
     Function to fetch a particular listing and its reviews
